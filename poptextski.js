ea5d6b00b582fefc89d12fa4e065eb111d9f6683
naveenvpai <pai.naveen357@gmail.com>
2020-06-23 22:41:58 -0700

    Fix paragraph breaks

--- poptextski.js
+++ poptextski.js
@@ -18,10 +18,15 @@ On the white below George dipped and rose and dipped out of sight. The rush and 
 
 <p>
 George stood a little farther down the slope, knocking the snow from his wind jacket with big slaps.
+</p><p>
 “You took a beauty, Mike,” he called to Nick. “That’s lousy soft snow. It bagged me the same way.”
+</p><p>
 “What’s it like over the khud?” Nick kicked his skis around as he lay on his back and stood up.
+</p><p>
 “You’ve got to keep to your left. It’s a good fast drop with a Christy at the bottom on account of a fence.”
+</p><p>
 “Wait a sec and we’ll take it together.”
+</p><p>
 “No, you come on and go first. I like to see you take the khuds.”
 </p>
 
@@ -43,7 +48,9 @@ He looked up the hill. George was coming down in telemark position, kneeling; on
 
 <p>
 “I was afraid to Christy,” George said, “the snow was too deep. You made a beauty.”
+</p><p>
 “I can’t telemark with my leg,” Nick said.
+</p><p>
 Nick held down the top strand of the wire fence with his ski and George slid over. Nick followed him down to the road. They thrust bent-kneed along the road into a pine forest. The road became polished ice, stained orange and a tobacco yellow from the teams hauling logs. The skiers kept to the stretch of snow along the side. The road dipped sharply to a stream and then ran straight up-hill. Through the woods they could see a long, low-eaved, weather-beaten building. Through the trees it was a faded yellow. Closer the window frames were painted green. The paint was peeling.Nick knocked his clamps loose with one of his ski sticks and kicked off the skis.
 </p>
 
@@ -51,6 +58,7 @@ Nick held down the top strand of the wire fence with his ski and George slid ove
 
 <p>
 “We might as well carry them up here,” he said.
+</p><p>
 He climbed the steep road with the skis on his shoulder, kicking his heel nails into the icy footing. He heard George breathing and kicking in his heels just behind him. They stacked the skis against the side of the inn and slapped the snow off each other’s trousers, stamped their boots clean,and went in.
 </p>
 
@@ -66,11 +74,16 @@ He climbed the steep road with the skis on his shoulder, kicking his heel nails 
 
 
 <p>
-Inside it was quite dark. A big porcelain stove shone in the corner of the room. There was a low ceiling. Smooth benches back of dark, wine-stained tables were along each side of the rooms. TwoSwiss sat over their pipes and two decies of cloudy new wine next to the stove. The boys took off their jackets and sat against the wall on the other side of the stove. A voice in the next room stopped singing and a girl in a blue apron came in through the door to see what they wanted to drink.
+Inside it was quite dark. A big porcelain stove shone in the corner of the room. There was a low ceiling. Smooth benches back of dark, wine-stained tables were along each side of the rooms. Two Swiss sat over their pipes and two decies of cloudy new wine next to the stove. The boys took off their jackets and sat against the wall on the other side of the stove. A voice in the next room stopped singing and a girl in a blue apron came in through the door to see what they wanted to drink.
+</p><p>
 “A bottle of Sion,” Nick said. “Is that all right, Gidge?”
+</p><p>
 “Sure,” said George. “You know more about wine than I do. I like any of it.”
+</p><p>
 The girl went out.
+</p><p>
 “There’s nothing really can touch skiing, is there?” Nick said. “The way it feels when you first drop off on a long run.”
+</p><p>
 “Huh,” said George. “It’s too swell to talk about.”
 </p>
 
@@ -83,7 +96,9 @@ The girl went out.
 
 <p>
 The girl brought the wine in and they had trouble with the cork. Nick finally opened it. The girl went out and they heard her singing in German in the next room.“Those specks of cork in it don’t matter,” said Nick.
+</p><p>
 “I wonder if she’s got any cake.”
+</p><p>
 “Let’s find out.”
 </p>
 
@@ -93,11 +108,17 @@ The girl brought the wine in and they had trouble with the cork. Nick finally op
 
 <p>
 The girl came in and Nick noticed that her apron covered swellingly her pregnancy. I wonder why I didn’t see that when she first came in, he thought.
+</p><p>
 “What were you singing?” he asked her.
+</p><p>
 “Opera, German opera.” She did not care to discuss the subject. “We have some apple strudel if you want it.”
+</p><p>
 “She isn’t so cordial, is she?” said George.
+</p><p>
 “Oh, well. She doesn’t know us and she thought we were going to kid her about her singing,maybe. She’s from up where they speak German probably and she’s touchy about being here and then she’s got that baby coming without being married and she’s touchy.”
+</p><p>
 “How do you know she isn’t married?”
+</p><p>
 “No ring. Hell, no girls get married around here till they’re knocked up.”
 </p>
 
@@ -117,9 +138,13 @@ The door came open and a gang of woodcutters from up the road came in, stamping 
 
 <p>
 George and Nick were happy. They were fond of each other. They knew they had the run back home ahead of them.
+</p><p>
 “When have you got to go back to school?” Nick asked.
+</p><p>
 “Tonight,” George answered. “I’ve got to get the ten-forty from Montreux.”
+</p><p>
 “I wish you could stick over and we could do the Dent du Lys tomorrow.”
+</p><p>
 “I got to get educated,” George said. “Gee, Mike, don’t you wish we could just bum together?
 </p>
 
@@ -130,48 +155,91 @@ George and Nick were happy. They were fond of each other. They knew they had the
 
 <p>
 Take our skis and go on the train to where there was good running and then go on and put up at pubs and go right across the Oberland and up the Valais and all through the Engadine and just take repair kit and extra sweaters and pyjamas in our rucksacks and not give a damn about school or anything.”
+</p><p>
 “Yes, and go through the Schwarzwald that way. Gee, the swell places.”“That’s where you went fishing last summer, isn’t it?”
+</p><p>
 “Yes.”
+</p><p>
 They ate the strudel and drank the rest of the wine.
+</p><p>
 George leaned back against the wall and shut his eyes.“Wine always makes me feel this way,” he said.“Feel bad?” Nick asked.
+</p><p>
 “No. I feel good, but funny.”
+</p><p>
 “I know,” Nick said.
+</p><p>
 “Sure,” said George.
+</p><p>
 “Should we have another bottle?” Nick asked.
+</p><p>
 “Not for me,” George said.
+</p><p>
 They sat there, Nick leaning his elbows on the table, George slumped back against the wall.“Is Helen going to have a baby?” George said, coming down to the table from the wall.“Yes.”
+</p><p>
 “When?”
+</p><p>
 “Late next summer.”
+</p><p>
 “Are you glad?”
+</p><p>
 “Yes. Now.”
+</p><p>
 “Will you go back to the States?”
+</p><p>
 “I guess so.”
+</p><p>
 “Do you want to?”
+</p><p>
 “No.”
+</p><p>
 “Does Helen?”
+</p><p>
 “No.”
+</p><p>
 George sat silent. He looked at the empty bottle and the empty glasses.
+</p><p>
 “It’s hell, isn’t it?” he said.
+</p><p>
 “No. Not exactly,” Nick said.
+</p><p>
 “Why not?”
+</p><p>
 “I don’t know,” Nick said.
+</p><p>
 “Will you ever go skiing together in the States?” George said.
+</p><p>
 “I don’t know,” said Nick.
+</p><p>
 “The mountains aren’t much,” George said.
+</p><p>
 “No,” said Nick. “They’re too rocky. There’s too much timber and they’re too far away.”“Yes,” said George, “that’s the way it is in California.”
+</p><p>
 “Yes,” Nick said, “that’s the way it is everywhere I’ve ever been.”
+</p><p>
 “Yes,” said George, “that’s the way it is.”
+</p><p>
 The Swiss got up and paid and went out.
+</p><p>
 “I wish we were Swiss,” George said.
+</p><p>
 “They’ve all got goiter,” said Nick.“I don’t believe it,” George said.“Neither do I,” said Nick.
+</p><p>
 They laughed.
+</p><p>
 “Maybe we’ll never go skiing again, Nick,” George said.
+</p><p>
 “We’ve got to,” said Nick. “It isn’t worth while if you can’t.”
+</p><p>
 “We’ll go, all right,” George said.
+</p><p>
 “We’ve got to,” Nick agreed.
+</p><p>
 “I wish we could make a promise about it,” George said.
+</p><p>
 Nick stood up. He buckled his wind jacket tight. He leaned over George and picked up the two ski poles from against the wall. He stuck one of the ski poles into the floor.
+</p><p>
 “There isn’t any good in promising,” he said.
+</p><p>
 They opened the door and went out. It was very cold. The snow had crusted hard. The road ran up the hill into the pine trees.
 </p>
 
